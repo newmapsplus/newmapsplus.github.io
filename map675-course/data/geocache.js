f8f06696e95b25286ef995db40e286fd75391651
Boyd <boydx@users.noreply.github.com>
2025-05-17 08:32:15 -0400

    add course syllabus

---
+++ map675-course/data/geocache.js
@@ -0,0 +1,19 @@
+const locations = {
+    "features": [
+
+    { "type": "Feature", "properties": { "amenity": "waterfountain.svg", "name": "Thoroughbred Park", "pop_up": "#" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -84.490400, 38.041601] ] } },
+    { "type": "Feature", "properties": { "amenity": "waterfountain.svg", "name": "Courthouse", "pop_up": "#" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -84.495663, 38.046927] ] } },
+    { "type": "Feature", "properties": { "amenity": "waterfountain.svg", "name": "Limestone Curtain", "pop_up": "#" }, "geometry": { "type": "MultiPoint", "coordinates": [ [-84.496743, 38.046258] ] } },
+    { "type": "Feature", "properties": { "amenity": "waterfountain.svg", "name": "Phoenix Park", "pop_up": "#" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -84.497247,38.045936] ] } }, 
+    { "type": "Feature", "properties": { "amenity": "waterfountain.svg", "name": "FifthThird", "pop_up": "#" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -84.499451,38.047544] ] } }, 
+    { "type": "Feature", "properties": { "amenity": "waterfountain.svg", "name": "Triangle Park", "pop_up": "#" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -84.500739, 38.048949] ] } }, 
+    { "type": "Feature", "properties": { "amenity": "waterfountain.svg", "name": "Chase", "pop_up": "#" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -84.494685, 38.044567] ] } }, 
+    
+    
+    { "type": "Feature", "properties": { "amenity": "rainbow.svg", "name": "Old wise one, knobbly and gnarled", "pop_up": "#" }, "geometry": { "type": "MultiPoint", "coordinates": [ [-84.500427, 38.032274] ] } }, 
+    { "type": "Feature", "properties": { "amenity": "pizza.svg", "name": "Free Pizza and Geography Awareness Week!", "pop_up": "#" }, "geometry": { "type": "MultiPoint", "coordinates": [ [-84.503370,38.043482] ] } }, 
+    { "type": "Feature", "properties": { "amenity": "ky_2.svg", "name": "UKy Department of Geography<br><span style='font-size:0.8em;'>75th Anniversary — <a href='https://uky-gis.github.io'>visit</a></span>", "pop_up": "#" }, "geometry": { "type": "MultiPoint", "coordinates": [ [-84.504280, 38.038659] ] } }
+    
+
+    ]
+    };
